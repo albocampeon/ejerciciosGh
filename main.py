d4e3a3f7153bcedabef446b702fc2256c344a2e2
Sergio Puebla <shispa_gb@live.cl>
2024-04-12 20:54:17 -0400

    Este es mi primer commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#Ejercicio 1 y 2
+
+print("Bienvenido al mundo de la programacion")
+nom = input ("Para comenzar ingresa tu nombre")
+print (f"Bienvenido{nom}")
+
+#Ejercicio 3
+
+print("Ingresa un valor 'x' y podrás resolver la siguiente ecuación")
+x = int(input("(x^2+3*x+1)/4"))
+formula = (x^2+3*x+1)/4
+print(f"El resultado de la operación con el valor {x} es: {formula}", formula)
+
+#Ejercicio 4
+
+nombre = input("Ingrese su nombre\n")
+rut = input("Ingrese su rut con puntos y guion")
+correo = input("Ingrese su correo")
+telefono = input("Ingrese su numero telefonico")
+dat = "Datos personales"
+print(dat.upper)())
+print("NOMBRE: \t", nombre.upper())
+print("RUT: \t\t", rut)
+print("CORREO:"
