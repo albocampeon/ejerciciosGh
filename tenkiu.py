5f34744a13a0acb93de1c2bac391e9fcbcbbe6ca
maruchandepollo <shispa_gb@live.cl>
2024-04-22 21:55:51 -0400

    xd

---
+++ tenkiu.py
@@ -0,0 +1,38 @@
+#Actividad 2
+#Ejercicio 1
+contadorProductos = 0
+acumuladorPrecio = 0
+opcion = int(input("¿Desea llevar agua por $600?    1-.Si   2-.No\n"))
+if opcion == 1:
+    contadorProductos += 1
+    acumuladorPrecio += 600
+opcion = int(input("¿Desea llevar azucar por $1200?   1-.Si   2-.No\n"))
+if opcion == 1:
+    contadorProductos += 1
+    acumuladorPrecio += 1200
+opcion = int(input("¿Desea llevar aceite por $1500?   1-.Si   2-.No\n"))
+if opcion ==1:
+    contadorProductos += 1   
+    acumuladorPrecio += 1200
+opcion = int(input("¿Desea llevar arroz por $1250?   1-.Si   2-.No\n"))
+if opcion ==1:
+    contadorProductos += 1
+    acumuladorPrecio +=1250
+opcion = int(input("¿Desea llevar fideos por $790?   1-.Si   2-.No\n"))
+if opcion ==1:
+    contadorProductos += 1
+    acumuladorPrecio += 790
+opcion = int(input("¿Desea llevar bebida por $1780?   1-.Si   2-.No\n"))
+if opcion ==1:
+    contadorProductos += 1
+    acumuladorPrecio += 1780
+opcion = int(input("¿Desea llevar chocolate por $2500?   1-.Si   2-.No\n"))
+if opcion ==1:
+    contadorProductos += 1
+    acumuladorPrecio += 2500
+opcion = int(input("¿Desea llevar pan de molde por $1340?   1-.Si   2-.No"))
+if opcion ==1:
+    contadorProductos += 1
+    acumuladorPrecio += 1340
+    
+                                 
